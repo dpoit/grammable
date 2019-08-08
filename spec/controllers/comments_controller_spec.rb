8cab37396a52bf9f8a5150a99238f813d7ea3d2a
dpoit <dannypoit@gmail.com>
2019-08-08 04:12:23 -0400

    Modified formatting in comments controller spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe CommentsController, type: :controller do
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+      post :create, params: { 
+        gram_id: gram.id, 
+        comment: { message: 'awesome gram' } 
+      }
 
       expect(response).to redirect_to root_path
       expect(gram.comments.length).to eq 1
@@ -17,14 +20,20 @@ RSpec.describe CommentsController, type: :controller do
 
     it "should require a user to be logged in to comment on a gram" do
       gram = FactoryBot.create(:gram)
-      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
+      post :create, params: { 
+        gram_id: gram.id, 
+        comment: { message: 'awesome gram' } 
+      }
       expect(response).to redirect_to new_user_session_path
     end
 
     it "should return http status code of not found if the gram isn't found" do
       user = FactoryBot.create(:user)
       sign_in user
-      post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' } }
+      post :create, params: { 
+        gram_id: 'YOLOSWAG', 
+        comment: { message: 'awesome gram' } 
+      }
       expect(response).to have_http_status(:not_found)
     end
   end
